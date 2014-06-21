a182782d5c40e0076f4168ccd201a6495837a5da
jickpictures <ryan@jickpictures.com>
2014-06-21 12:51:09 -0700

    Added IFTTT app

---
+++ IFTTT/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  IFTTT
+//
+//  Created by Ryan Gordon on 5/10/14.
+//  Copyright (c) 2014 Jick Pictures. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
+
