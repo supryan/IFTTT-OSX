f99c7d5a9d5ce1e150e9ac1574626bb186679164
jickpictures <ryan@jickpictures.com>
2014-09-07 17:26:50 -0700

    Added LoseIt notifications and associated assets

---
+++ LoseIt/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  LoseIt
+//
+//  Created by Ryan Gordon on 9/7/14.
+//  Copyright (c) 2014 Jick Pictures. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
